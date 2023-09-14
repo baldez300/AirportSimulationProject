b96195d969dd3c44de73a8c5ddffbbbb20fd45b1
Balde641 <62099893+Balde641@users.noreply.github.com>
2023-09-14 15:35:49 +0300

    Asiakas ominaisuus päivitetty (Asiakasluokan sisällä)

--- src/simu/model/Asiakas.java
+++ src/simu/model/Asiakas.java
@@ -2,25 +2,22 @@ package simu.model;
 
 import simu.framework.*;
 
-// TODO:
-// Asiakas koodataan simulointimallin edellyttämällä tavalla (data!)
-
 public class Asiakas {
 	private double saapumisaika;
 	private double poistumisaika;
 	private int id;
 	private static int i = 1;
 	private static long sum = 0;
-	
-	// TODO BALDE:
-	// Arvotaan asiakkaalle ominaisuus joka määrää onko asiakas
-	// ulkomaan vai kotimaanlennon asiakas (Asiakasluokan sisällä)
+	private boolean ulkomaanlento; // Uusi kenttä, määrittää onko asiakas ulkomaanlennon asiakas
 
-	public Asiakas(){
-	    id = i++;
-
+	public Asiakas() {
+		id = i++;
 		saapumisaika = Kello.getInstance().getAika();
-		Trace.out(Trace.Level.INFO, "Uusi asiakas nro " + id + " saapui klo "+saapumisaika);
+
+		// Arvotaan ulkomaanlennon asiakkuus
+		ulkomaanlento = arvoUlkomaanlento();
+
+		Trace.out(Trace.Level.INFO, "Uusi asiakas nro " + id + " saapui klo " + saapumisaika);
 	}
 
 	public double getPoistumisaika() {
@@ -38,21 +35,30 @@ public class Asiakas {
 	public void setSaapumisaika(double saapumisaika) {
 		this.saapumisaika = saapumisaika;
 	}
-	
-
 
 	public int getId() {
 		return id;
 	}
-	
-	public void raportti(){
-		Trace.out(Trace.Level.INFO, "\nAsiakas "+id+ " valmis! ");
-		Trace.out(Trace.Level.INFO, "Asiakas "+id+ " saapui: " +saapumisaika);
-		Trace.out(Trace.Level.INFO,"Asiakas "+id+ " poistui: " +poistumisaika);
-		Trace.out(Trace.Level.INFO,"Asiakas "+id+ " viipyi: " +(poistumisaika-saapumisaika));
-		sum += (poistumisaika-saapumisaika);
-		double keskiarvo = sum/id;
-		System.out.println("Asiakkaiden läpimenoaikojen keskiarvo tähän asti "+ keskiarvo);
+
+	public boolean isUlkomaanlento() {
+		return ulkomaanlento;
 	}
 
+	// Arpoa ulkomaanlennon asiakkuuden
+	// Jos satunnaisluku on pienempi kuin 0.5 väliltä 0.0 ja 1.0, Math.random() < 0.5 palauttaa true.
+	// Jos satunnaisluku on suurempi tai yhtä suuri kuin 0.5, se palauttaa false.
+
+	private boolean arvoUlkomaanlento() {
+		return Math.random() < 0.5; // Voit muuttaa arvontakriteeriä tarpeidesi mukaan
+	}
+
+	public void raportti() {
+		Trace.out(Trace.Level.INFO, "\nAsiakas " + id + " valmis! ");
+		Trace.out(Trace.Level.INFO, "Asiakas " + id + " saapui: " + saapumisaika);
+		Trace.out(Trace.Level.INFO, "Asiakas " + id + " poistui: " + poistumisaika);
+		Trace.out(Trace.Level.INFO, "Asiakas " + id + " viipyi: " + (poistumisaika - saapumisaika));
+		sum += (poistumisaika - saapumisaika);
+		double keskiarvo = sum / id;
+		System.out.println("Asiakkaiden läpimenoaikojen keskiarvo tähän asti " + keskiarvo);
+	}
 }
