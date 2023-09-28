2789a229289ae9be2b1460fc8e97f92a7b015290
Samigo83 <sami.paananen@gmail.com>
2023-09-28 13:00:23 +0300

    Interface tietokantaa varten

--- src/main/java/simu/framework/IDao.java
+++ src/main/java/simu/framework/IDao.java
@@ -3,10 +3,10 @@ package simu.framework;
 import simu.entity.*;
 
 import java.util.List;
+import java.util.HashMap;
 
 public interface IDao {
     void tallenna(Tulokset tulos, LSTulos pptulos, TTTulos tttulos, PTTulos pttulos, T1Tulos t1tulos, T2Tulos t2tulos);
-    void poista(Tulokset tulos);
     List<Tulokset> lataaKaikki();
-    Tulokset lataaYksi();
+    HashMap<Object, Object> lataaTarkemmatTiedot(int id);
 }
