af3f2fd192c6ee2c82a044d05acc27581281cc49
Samigo83 <sami.paananen@gmail.com>
2023-10-02 10:33:20 +0300

    Muokataan nimiä

--- src/main/java/simu/model/TapahtumanTyyppi.java
+++ src/main/java/simu/model/TapahtumanTyyppi.java
@@ -3,8 +3,8 @@ package simu.model;
 import simu.framework.ITapahtumanTyyppi;
 
 /*  ARR1 = Saapuva asiakas ulkomaalle, ARR2 = Saapuva asiakas sisälennolle, DEP1 = Lähtöselvitys poistuminen, DEP2 = Turvatarkastus poistuminen
-	DEP3 = Passintarkistus poistuminen, DEP4 = Lähtöportti ulkomaat, DEP5 = Lähtöportti kotimaa
-	ULKO = Ulkomaan lento, SISA = Sisämaan lento
+	DEP3 = Passintarkistus poistuminen, DEP4 = Lähtöportti kotimaa, DEP5 = Lähtöportti ulkomaa,
+	ULKO = Ulkomaanlento, SISA = Kotimaanlento
 */
 
 public enum TapahtumanTyyppi implements ITapahtumanTyyppi{
