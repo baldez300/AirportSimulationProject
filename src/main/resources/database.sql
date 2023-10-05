56ae00f2d736b16b289576717a567d43c16f2b22
Samigo83 <sami.paananen@gmail.com>
2023-10-05 23:21:11 +0300

    lisätään taulut tiedoille koska generation-schema = none. aja uusi tietokanta koneelle jos taulut puuttuvat

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -4,3 +4,55 @@ USE ap_simulaattori;
 DROP USER IF EXISTS 'appuser'@'localhost';
 CREATE USER 'appuser'@'localhost' IDENTIFIED BY 'appuser123';
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, ALTER, DROP ON ap_simulaattori.* TO 'appuser'@'localhost';
+
+CREATE TABLE lahtoselvitys(
+    id INT NOT NULL AUTO_INCREMENT,
+    jononpituus DOUBLE,
+    jonotusaika DOUBLE,
+    kayttoaste DOUBLE,
+    suoritusteho DOUBLE,
+    PRIMARY KEY (id)
+);
+CREATE TABLE passintarkastus(
+    id INT NOT NULL AUTO_INCREMENT,
+    jononpituus DOUBLE,
+    jonotusaika DOUBLE,
+    kayttoaste DOUBLE,
+    suoritusteho DOUBLE,
+    PRIMARY KEY (id)
+);
+CREATE TABLE t1(
+    id INT NOT NULL AUTO_INCREMENT,
+    jononpituus DOUBLE,
+    jonotusaika DOUBLE,
+    kayttoaste DOUBLE,
+    suoritusteho DOUBLE,
+    PRIMARY KEY (id)
+);
+CREATE TABLE t2(
+    id INT NOT NULL AUTO_INCREMENT,
+    jononpituus DOUBLE,
+    jonotusaika DOUBLE,
+    kayttoaste DOUBLE,
+    suoritusteho DOUBLE,
+    PRIMARY KEY (id)
+);
+CREATE TABLE tulokset(
+    tulokset_id INT NOT NULL AUTO_INCREMENT,
+    aika DOUBLE,
+    asiakkaat DOUBLE,
+    lennolle_ehtineet DOUBLE,
+    myohastyneet DOUBLE,
+    myohastyneet_t1 DOUBLE,
+    myohastyneet_t2 DOUBLE,
+    paivamaara DATE,
+    PRIMARY KEY (tulokset_id)
+);
+CREATE TABLE turvatarkastus(
+    id INT NOT NULL AUTO_INCREMENT,
+    jononpituus DOUBLE,
+    jonotusaika DOUBLE,
+    kayttoaste DOUBLE,
+    suoritusteho DOUBLE,
+    PRIMARY KEY (id)
+);
