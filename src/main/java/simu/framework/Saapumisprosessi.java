d5e63c5d30c8674fcf29bda9aab2ccd496d845f7
Samigo83 <sami.paananen@gmail.com>
2023-10-05 12:01:30 +0300

    muokataan kommentteja kuvaamaan paremmin tapahtumaa

--- src/main/java/simu/framework/Saapumisprosessi.java
+++ src/main/java/simu/framework/Saapumisprosessi.java
@@ -11,7 +11,6 @@ public class Saapumisprosessi {
 	private double ulkoLahtoAika, lentojenVali;
 
 	public Saapumisprosessi(Tapahtumalista tl, ITapahtumanTyyppi tyyppi, double ulkoLahtoAika, double lentojenVali) {
-		//this.generaattori = g;
 		this.tapahtumalista = tl;
 		this.tyyppi = tyyppi;
 		this.ulkoLahtoAika = ulkoLahtoAika;
@@ -20,27 +19,27 @@ public class Saapumisprosessi {
 
 	public void generoiSeuraava() {
 		
-		// Lähtöaika ulkomaalennoille
+		// Lähtöaika ulkomaan lennoille
 		double ulkoLahtoAika = Kello.getInstance().getAika() + new Normal(this.ulkoLahtoAika, 5).sample();
 
-		// Lähtöaika sisälennoille
-		double lahtoAika = ulkoLahtoAika + this.lentojenVali; //new Normal(this.lentojenVali, 5).sample() ;
+		// Lähtöaika kotimaan lennoille
+		double lahtoAika = ulkoLahtoAika + this.lentojenVali;
 
-		// Luodaan tapahtuma "Ulkomaan lento"
+		// Luodaan tapahtuma ulkomaan lento
 		Tapahtuma tUlko = new Tapahtuma(tyyppi, ulkoLahtoAika, true);
 		tapahtumalista.lisaa(tUlko);
 
-		// Luodaan tapahtuma "Sisämaan lento"
+		// Luodaan tapahtuma kotimaan lento
 		Tapahtuma tSisa = new Tapahtuma(SISA, lahtoAika, false);
 		tapahtumalista.lisaa(tSisa);
 
-		// Luodaan 10 tapahtumaa "Saapuva asiakas ulkomaalle"
+		// Luodaan 10 tapahtumaa (saapuvaa asiakasta) ulkomaan lennolle
 		for (int i=0; i<10; i++) {
 			Tapahtuma t1 = new Tapahtuma(ARR1, ulkoLahtoAika - (new Normal(240, 15).sample()), true);
 			tapahtumalista.lisaa(t1);
 		}
 
-		// Luodaan 10 tapahtumaa "Saapuva asiakas sisälennolle"
+		// Luodaan 10 tapahtumaa (saapuvaa asiakasta) kotimaan lennolle
 		for (int i=0; i<10; i++) {
 			Tapahtuma t2 = new Tapahtuma(ARR2, lahtoAika - (new Normal(240, 15).sample()), false);
 			tapahtumalista.lisaa(t2);
